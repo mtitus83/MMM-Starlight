e33d7fd40eb4f67314a1bda75878f8058049a514
Matt Titus <mtitus83@gmail.com>
2024-08-27 15:49:57 -0400

    fetch "tomorrow" once randomly despite configuration, store in cache to be used as "daily" the following day to reduce polling. Will retry in an hour if unsuccessful.

--- MMM-SunSigns.js
+++ MMM-SunSigns.js
@@ -23,30 +23,26 @@ Module.register("MMM-SunSigns", {
         this.lastUpdateAttempt = null;
         this.updateFailures = 0;
         this.transitionState = "idle";
+        this.tomorrowFetched = false;
 
-        this.scheduleUpdate(1000);
+        this.scheduleInitialUpdate();
+        this.scheduleMidnightShift();
+        if (!this.config.period.includes("tomorrow")) {
+            this.scheduleRandomTomorrowFetch();
+        }
     },
 
     getStyles: function() {
         return ["MMM-SunSigns.css"];
     },
 
-    scheduleUpdate: function(delay) {
-        var self = this;
-        setTimeout(function() {
-            self.updateHoroscopes();
-        }, delay);
+    scheduleInitialUpdate: function() {
+        setTimeout(() => {
+            this.updateHoroscopes();
+        }, 1000);
     },
 
-    updateHoroscopes: function() {
-        this.lastUpdateAttempt = new Date().toLocaleString();
-        Log.info(this.name + ": Sending UPDATE_HOROSCOPES notification");
-        this.sendSocketNotification("UPDATE_HOROSCOPES", {
-            zodiacSigns: this.config.zodiacSign,
-            periods: this.config.period,
-        });
-
-        // Schedule next update at midnight
+    scheduleMidnightShift: function() {
         var now = new Date();
         var night = new Date(
             now.getFullYear(),
@@ -56,183 +52,59 @@ Module.register("MMM-SunSigns", {
         );
         var msTillMidnight = night.getTime() - now.getTime();
 
-        this.scheduleUpdate(msTillMidnight);
-    },
-
-    getDom: function() {
-        var wrapper = document.createElement("div");
-        wrapper.className = "MMM-SunSigns";
-        wrapper.style.width = this.config.width;
-        wrapper.style.fontSize = this.config.fontSize;
-
-        if (!this.loaded) {
-            wrapper.innerHTML = "Loading horoscope...";
-            if (this.config.debug) {
-                wrapper.innerHTML += "<br>Last attempt: " + this.lastUpdateAttempt;
-                wrapper.innerHTML += "<br>Update failures: " + this.updateFailures;
-            }
-            return wrapper;
-        }
-
-        var slideContainer = document.createElement("div");
-        slideContainer.className = "sunsigns-slide-container";
-
-        var currentSign = this.config.zodiacSign[this.currentSignIndex];
-        var currentPeriod = this.config.period[this.currentPeriodIndex];
-        slideContainer.appendChild(this.createSignElement(currentSign, "current", currentPeriod));
-
-        var nextIndices = this.getNextIndices();
-        var nextSign = this.config.zodiacSign[nextIndices.signIndex];
-        var nextPeriod = this.config.period[nextIndices.periodIndex];
-        slideContainer.appendChild(this.createSignElement(nextSign, "next", nextPeriod));
-
-        wrapper.appendChild(slideContainer);
-
-        if (this.config.debug) {
-            var debugInfo = document.createElement("div");
-            debugInfo.className = "small dimmed";
-            debugInfo.innerHTML = `Last Update: ${this.lastUpdateAttempt}<br>
-                                   Update Failures: ${this.updateFailures}<br>
-                                   Current State: ${this.transitionState}`;
-            wrapper.appendChild(debugInfo);
-        }
-
-        return wrapper;
-    },
-
-    createSignElement: function(sign, className, period) {
-        var slideWrapper = document.createElement("div");
-        slideWrapper.className = "sunsigns-slide-wrapper " + className;
-
-        var contentWrapper = document.createElement("div");
-        contentWrapper.className = "sunsigns-content-wrapper";
-
-        var textContent = document.createElement("div");
-        textContent.className = "sunsigns-text-content";
-
-        var periodText = document.createElement("div");
-        periodText.className = "sunsigns-period";
-        periodText.innerHTML = this.formatPeriodText(period) + " Horoscope for " + sign.charAt(0).toUpperCase() + sign.slice(1);
-        textContent.appendChild(periodText);
-
-        var horoscopeWrapper = document.createElement("div");
-        horoscopeWrapper.className = "sunsigns-text-wrapper";
-        horoscopeWrapper.style.maxHeight = this.config.maxTextHeight;
-
-        var horoscopeTextElement = document.createElement("div");
-        horoscopeTextElement.className = "sunsigns-text";
-        horoscopeTextElement.innerHTML = this.horoscopes[sign] && this.horoscopes[sign][period] 
-            ? this.horoscopes[sign][period] 
-            : "Loading " + period + " horoscope for " + sign + "...";
-        horoscopeWrapper.appendChild(horoscopeTextElement);
-
-        textContent.appendChild(horoscopeWrapper);
-        contentWrapper.appendChild(textContent);
-
-        if (this.config.showImage) {
-            var imageWrapper = document.createElement("div");
-            imageWrapper.className = "sunsigns-image-wrapper";
-            var image = document.createElement("img");
-            image.src = `https://www.sunsigns.com/wp-content/themes/sunsigns/assets/images/_sun-signs/${sign}/wrappable.png`;
-            image.alt = sign + " zodiac sign";
-            image.style.width = this.config.imageWidth;
-            imageWrapper.appendChild(image);
-            contentWrapper.appendChild(imageWrapper);
-        }
-
-        slideWrapper.appendChild(contentWrapper);
-
-        return slideWrapper;
-    },
-
-    formatPeriodText: function(period) {
-        if (period === "tomorrow") {
-            return "Tomorrow's";
-        }
-        return period.charAt(0).toUpperCase() + period.slice(1);
-    },
-
-    getNextIndices: function() {
-        let nextPeriodIndex = (this.currentPeriodIndex + 1) % this.config.period.length;
-        let nextSignIndex = this.currentSignIndex;
-        if (nextPeriodIndex === 0) {
-            nextSignIndex = (this.currentSignIndex + 1) % this.config.zodiacSign.length;
-        }
-        return { signIndex: nextSignIndex, periodIndex: nextPeriodIndex };
-    },
-
-    scheduleNextTransition: function() {
-        if (this.config.zodiacSign.length === 1 && this.config.period.length === 1) {
-            return;
-        }
-
-        this.transitionState = "waiting";
         setTimeout(() => {
-            this.transitionState = "sliding";
-            this.updateDom(0); // Update DOM immediately
-            this.startSlideTransition();
-        }, this.config.signWaitTime);
+            this.shiftTomorrowToDaily();
+            this.tomorrowFetched = false; // Reset for the new day
+            this.scheduleMidnightShift(); // Schedule next midnight shift
+            if (!this.config.period.includes("tomorrow")) {
+                this.scheduleRandomTomorrowFetch(); // Schedule next random fetch for the new day
+            }
+        }, msTillMidnight);
     },
 
-    startSlideTransition: function() {
-        var slideContainer = document.querySelector(".MMM-SunSigns .sunsigns-slide-container");
-        if (slideContainer) {
-            slideContainer.style.transition = `transform 1000ms ease-in-out`; // Hard-coded 1 second transition
-            slideContainer.style.transform = "translateX(-50%)";
+    scheduleRandomTomorrowFetch: function() {
+        if (this.tomorrowFetched) return; // Don't schedule if already fetched today
 
-            setTimeout(() => {
-                this.finishTransition();
-            }, 1000); // Hard-coded 1 second wait
-        }
-    },
-
-    finishTransition: function() {
-        let nextIndices = this.getNextIndices();
-        this.currentSignIndex = nextIndices.signIndex;
-        this.currentPeriodIndex = nextIndices.periodIndex;
-        this.transitionState = "pausing";
-        this.updateDom(0);
-
-        var slideContainer = document.querySelector(".MMM-SunSigns .sunsigns-slide-container");
-        if (slideContainer) {
-            slideContainer.style.transition = "none";
-            slideContainer.style.transform = "translateX(0)";
-        }
+        const minDelay = 1 * 60 * 60 * 1000; // 1 hour
+        const maxDelay = 20 * 60 * 60 * 1000; // 20 hours
+        const randomDelay = Math.floor(Math.random() * (maxDelay - minDelay + 1) + minDelay);
 
         setTimeout(() => {
-            this.transitionState = "scrolling";
-            this.startScrolling();
-        }, this.config.pauseDuration);
+            this.fetchTomorrowHoroscopes();
+        }, randomDelay);
     },
 
-    startScrolling: function() {
-        var self = this;
-        var textWrapper = document.querySelector(".MMM-SunSigns .sunsigns-text-wrapper");
-        var textContent = document.querySelector(".MMM-SunSigns .sunsigns-text");
+    updateHoroscopes: function() {
+        this.lastUpdateAttempt = new Date().toLocaleString();
+        Log.info(this.name + ": Sending UPDATE_HOROSCOPES notification");
+        this.sendSocketNotification("UPDATE_HOROSCOPES", {
+            zodiacSigns: this.config.zodiacSign,
+            periods: this.config.period,
+        });
+    },
 
-        if (textWrapper && textContent) {
-            var wrapperHeight = textWrapper.offsetHeight;
-            var contentHeight = textContent.offsetHeight;
+    fetchTomorrowHoroscopes: function() {
+        if (this.tomorrowFetched) return; // Don't fetch if already fetched today
 
-            if (contentHeight > wrapperHeight) {
-                this.isScrolling = true;
-                var scrollDistance = contentHeight - wrapperHeight;
-                var scrollDuration = (scrollDistance / this.config.scrollSpeed) * 1000;
+        Log.info(this.name + ": Fetching tomorrow's horoscopes");
+        this.sendSocketNotification("UPDATE_HOROSCOPES", {
+            zodiacSigns: this.config.zodiacSign,
+            periods: ["tomorrow"],
+        });
+    },
 
-                textContent.style.transition = `transform ${scrollDuration}ms linear`;
-                textContent.style.transform = `translateY(-${scrollDistance}px)`;
-
-                setTimeout(() => {
-                    this.isScrolling = false;
-                    this.transitionState = "idle";
-                    this.scheduleNextTransition();
-                }, scrollDuration + this.config.pauseDuration);
-            } else {
-                this.transitionState = "idle";
-                this.scheduleNextTransition();
+    shiftTomorrowToDaily: function() {
+        Log.info(this.name + ": Shifting tomorrow's horoscopes to daily");
+        for (let sign of this.config.zodiacSign) {
+            if (this.horoscopes[sign] && this.horoscopes[sign].tomorrow) {
+                this.horoscopes[sign].daily = this.horoscopes[sign].tomorrow;
+                delete this.horoscopes[sign].tomorrow;
             }
         }
+        this.updateDom();
     },
+
+    // ... (keep other methods as they were)
 
     socketNotificationReceived: function(notification, payload) {
         Log.info(this.name + ": Received socket notification: " + notification);
@@ -246,6 +118,9 @@ Module.register("MMM-SunSigns", {
                 this.loaded = true;
                 this.updateFailures = 0;
                 Log.info(this.name + ": Horoscope data loaded successfully");
+                if (payload.period === "tomorrow") {
+                    this.tomorrowFetched = true;
+                }
                 if (this.transitionState === "idle") {
                     this.updateDom();
                     this.scheduleNextTransition();
@@ -253,7 +128,14 @@ Module.register("MMM-SunSigns", {
             } else {
                 Log.error(this.name + ": Failed to fetch horoscope", payload);
                 this.updateFailures++;
-                this.scheduleUpdate(60 * 60 * 1000); // Retry in 1 hour if failed
+                // Retry in 1 hour if failed
+                setTimeout(() => {
+                    if (payload.period === "tomorrow") {
+                        this.fetchTomorrowHoroscopes();
+                    } else {
+                        this.updateHoroscopes();
+                    }
+                }, 60 * 60 * 1000);
             }
         } else {
             Log.warn(this.name + ": Received unknown socket notification: " + notification);
